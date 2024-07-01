dc1dcafaf5b354c0900cc30ca5379f3814ed35d4
tea2x <tungpham@lecle.co.kr>
2024-07-01 18:34:01 +0700

    improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -819,35 +819,37 @@ const App = () => {
   function tabNavigator() {
     return (
       <div>
-        <svg
-          className="to-right-sign"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          id="angle-right"
-          width="30"
-          height="30"
-          onClick={onPrevious}
-        >
-          <path 
-            fill="#524540" 
-            d="M14.83,11.29,10.59,7.05a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41L12.71,12,9.17,15.54a1,1,0,0,0,0,1.41,1,1,0,0,0,.71.29,1,1,0,0,0,.71-.29l4.24-4.24A1,1,0,0,0,14.83,11.29Z"
-          ></path>
-        </svg>
-
-        <svg
-          className="to-left-sign"
-          xmlns="http://www.w3.org/2000/svg" 
-          viewBox="0 0 24 24"
-          id="angle-left"
-          width="30"
-          height="30"
-          onClick={onNext}
-        >
-          <path 
-            fill="#524540" 
-            d="M11.29,12l3.54-3.54a1,1,0,0,0,0-1.41,1,1,0,0,0-1.42,0L9.17,11.29a1,1,0,0,0,0,1.42L13.41,17a1,1,0,0,0,.71.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41Z"
-          ></path>
-        </svg>
+        {isNextPage ? (
+          <svg
+            className="to-right-sign"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            id="angle-right"
+            width="30"
+            height="30"
+            onClick={onPrevious}
+          >
+            <path 
+              fill="#524540" 
+              d="M14.83,11.29,10.59,7.05a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41L12.71,12,9.17,15.54a1,1,0,0,0,0,1.41,1,1,0,0,0,.71.29,1,1,0,0,0,.71-.29l4.24-4.24A1,1,0,0,0,14.83,11.29Z"
+            ></path>
+          </svg>
+        ) : (
+          <svg
+            className="to-left-sign"
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24"
+            id="angle-left"
+            width="30"
+            height="30"
+            onClick={onNext}
+          >
+            <path 
+              fill="#524540" 
+              d="M11.29,12l3.54-3.54a1,1,0,0,0,0-1.41,1,1,0,0,0-1.42,0L9.17,11.29a1,1,0,0,0,0,1.42L13.41,17a1,1,0,0,0,.71.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41Z"
+            ></path>
+          </svg>
+        )}
       </div>
     );
   }
